fba3572a39e0bb3aaac72aaa0c885a7743bf432e
quantumcoder <quantum.computing96@gmail.com>
2016-11-02 00:34:49 +0530

    Added UCT tree

--- UCTTree.h
+++ UCTTree.h
@@ -6,12 +6,17 @@
 class UCTTree {
 public:
 	UCTNode *rootNode;
+	State* startState;
 	const int NEW_STATE_EPOCHS = 1;
 	const int NUM_SIM = 5;
 	const int SIM_STATE_EPOCHS = 3;
 	const int TREE_ITER = 10;
 
 	UCTTree(State *startState);
+
+	vector<Action>& getActionSequence();
+
+	UCTNode* getTreeNode(vector<Action>& actions);
 
 	UCTNode *select();
 
@@ -20,6 +25,8 @@ public:
 	void simulate(UCTNode *node);
 
 	void run();
+
+	double runSimulation(State* startState, Action action, int epochs);
 };
 
 #endif //LIFT_AGENT_UCTTREE_H
